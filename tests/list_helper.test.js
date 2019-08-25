c18d2ac6152b21c501da6d2a3d8539b6b9b96ea6
Rudolfs <rudolfs.berzins.python@gmail.com>
2019-08-25 16:09:58 +0200

    Completed exercise 4.4 - helper functions and unit tests, step2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,51 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+
+describe('total likes', () => {
+  const listWithOneBlog = [{
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }]
+
+  const listWithManyBlogs = [{
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }, {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 10,
+    __v: 0
+  }, {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 15,
+    __v: 0
+  }]
+
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.totalLikes(listWithManyBlogs)).toBe(30)
+  })
+})
