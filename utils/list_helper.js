d982b2dc898e63cbf5478c63300df04e83662e23
Rudolfs <rudolfs.berzins.python@gmail.com>
2019-08-25 16:32:23 +0200

    Completed exercise 4.5 - helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,16 +2,33 @@ const dummy = (blogs) => {
   return 1
 }
 
+const reducer = (sum, item) => {
+  return sum + item.likes
+}
+
 const totalLikes = array => {
-  const reducer = (sum, item) => {
-    return sum + item.likes
-  }
   return array.length === 0 ?
     0 :
     array.reduce(reducer, 0)
 }
 
+const favoriteBlog = array => {
+  if (array.length === 0) {
+    return 0
+  }
+
+  const allLikes = array.map(item => item.likes)
+  const favorite = array[allLikes.indexOf(Math.max(...allLikes))]
+
+  return {
+    'title': favorite.title,
+    'author': favorite.author,
+    'likes': favorite.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
