7c52d89e8aba7f640da5fa4849c4e0b519157d82
Rudolfs <rudolfs.berzins.python@gmail.com>
2019-09-11 15:25:11 +0200

    Completed exercise 4.13 - Blog list expansions, step1

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -83,6 +83,26 @@ test('blog without title and url is not added and rejected', async () => {
   expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
 })
 
+test('succeeds with status code 204 if id is valid', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd.length).toBe(
+    helper.initialBlogs.length - 1
+  )
+
+  const titles = blogsAtEnd.map(r => r.title)
+
+  expect(titles).not.toContain(blogToDelete.title)
+
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
